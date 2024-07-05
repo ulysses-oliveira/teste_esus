a0b62044aced1d5ab94c35132003fb72ff57dcc6
Ulysses Oliveira <102372800+ulysses-lorde@users.noreply.github.com>
2024-07-05 12:19:57 -0300

    Add files via upload

---
+++ pgdb.php
@@ -0,0 +1,9 @@
+<?php
+define('PGSQL_HOST','localhost');
+define('PGSQL_USER','postgres');
+define('PGSQL_PASSWORD','esus'); // esus
+define('PGSQL_DB_NAME','esus'); // esus
+define('PGSQL_PORT','5433');
+$PGSQL_CTRL = false;
+$PGSQL_ERRO = false;
+?>
